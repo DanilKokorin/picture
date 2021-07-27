c707230af792725c0a5c9b390a4bfa2836c51059
Данил Кокорин <63406852+DanilKokorin@users.noreply.github.com>
2021-07-27 10:39:18 +0300

    Add files via upload

---
+++ picture.php
@@ -0,0 +1,6 @@
+<picture class="img img--bg img--bg2">
+    <source srcset="{{ asset('landing/images/png/img/bg--logoss.webp') }}" media="(min-width: 768px)" width="600" height="229" type="image/webp">
+    <source srcset="{{ asset('landing/images/png/img/m_bg--logoss.webp') }}" media="(max-width: 768px)" width="350" height="122" type="image/webp">
+    <source srcset="{{ asset('landing/images/png/img/m_bg--logoss.png') }}" media="(max-width: 768px)" width="350" height="122" type="image/png">
+    <img src="{{ asset('landing/images/png/img/bg--logoss.png') }}" width="600" height="229" loading="lazy"/>
+</picture> 
